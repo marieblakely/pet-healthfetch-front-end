b75ea4873ae7b999a844147cdbca65b33083b9c2
Marie Blakely <marieblakely@myyahoo.com>
2024-05-13 10:23:05 -0400

    update

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -8,32 +8,33 @@ import styles from './PetCard.module.css'
 
 const PetCard = ({ pet }) => {
   return (  
-    <>
+  <>
     <NavLink to={`/pets/${pet._id}`}>
       <article className={styles.container}>
-      <img src={pet.photo} alt="pet photo"/>
-      <header>
-          <span>
-            <h1>{pet.name}</h1>
-          </span>
+        <header>
+          <h1>{pet.name}</h1>
+          <img src={pet.photo} alt="pet photo"/>
+          <section>
+            <span>
+              <h5>{pet.phone}</h5>
+              <h5>{pet.address}</h5>
+              <h5>{pet.birthdate}</h5>
+              <h5>{pet.breed}</h5>
+              <h5>{pet.color}</h5>
+              <h5>{pet.sex}</h5>
+              <h5>{pet.elixir}</h5>
+              <h5>{pet.medicalHistory}</h5>
+              <h5>{pet.medications}</h5>
+              <h5>{pet.allergies}</h5>
+              <h5>{pet.vetName}</h5>
+            </span>
+          
+          </section>
           <OwnerInfo content={pet} />
         </header>
-          <section>
-            <h5>{pet.phone}</h5>
-            <h5>{pet.address}</h5>
-            <h5>{pet.birthdate}</h5>
-            <h5>{pet.breed}</h5>
-            <h5>{pet.color}</h5>
-            <h5>{pet.sex}</h5>
-            <h5>{pet.elixir}</h5>
-            <h5>{pet.medicalHistory}</h5>
-            <h5>{pet.medications}</h5>
-            <h5>{pet.allergies}</h5>
-            <h5>{pet.vetName}</h5>
-          </section>
       </article>
     </NavLink>
-    </>
+  </>
   )
 }
 
