4b082cf526ea47d14a7f48cc0210360e3b16876d
Marie Blakely <marieblakely@myyahoo.com>
2024-05-13 13:24:13 -0400

    import loading and ownerinfo components to petdetails, update return with pet details jsx, add loading conditional

--- src/pages/PetDetails/PetDetails.jsx
+++ src/pages/PetDetails/PetDetails.jsx
@@ -1,6 +1,11 @@
 //npm modules\
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
+//services
+import * as petService from '../../services/petService'
+//components
+import Loading from '../Loading/Loading'
+import OwnerInfo from '../../components/OwnerInfo/OwnerInfo'
 // css
 import styles from './PetDetails.module.css'
 
@@ -16,9 +21,26 @@ const PetDetails = (props) => {
     fetchPet()
   }, [petId])
 
+  if (!pet) return <Loading />
+
   return (  
     <main className={styles.container}>
-      PetDetails
+      <section>
+        <span>
+          <h5>{pet.phone}</h5>
+          <h5>{pet.address}</h5>
+          <h5>{pet.birthdate}</h5>
+          <h5>{pet.breed}</h5>
+          <h5>{pet.color}</h5>
+          <h5>{pet.sex}</h5>
+          <h5>{pet.elixir}</h5>
+          <h5>{pet.medicalHistory}</h5>
+          <h5>{pet.medications}</h5>
+          <h5>{pet.allergies}</h5>
+          <h5>{pet.vetName}</h5>
+        </span>
+        <OwnerInfo content={pet} />
+      </section>
     </main>
   )
 }
