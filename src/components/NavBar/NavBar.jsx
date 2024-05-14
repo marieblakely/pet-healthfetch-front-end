2d086ddd6c56babcc20aee8c0b7502beac5d5c06
Marie Blakely <marieblakely@myyahoo.com>
2024-05-13 21:33:51 -0400

    remove underline from navlinks

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,17 +13,17 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <ul>
           <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
+          <li><NavLink to="/profiles">PROFILES</NavLink></li>
           <li><NavLink to="/pets">PETS</NavLink></li>
           <li><NavLink to="/pets/new">NEW PET</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink>
+          <li><NavLink to="/auth/change-password">CHANGE PASSWORD</NavLink>
           </li>
         </ul>
       :
         <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
+          <li><NavLink to="/auth/login">LOG IN</NavLink></li>
+          <li><NavLink to="/auth/signup">SIGN UP</NavLink></li>
         </ul>
       }
     </nav>
