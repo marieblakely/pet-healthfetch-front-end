1c04709fd82044299b65b0f13aed606a182e0757
Marie Blakely <marieblakely@myyahoo.com>
2024-05-14 15:58:57 -0400

    adjust navbar, adjust height of body and add padding top to body

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -45,7 +45,7 @@ const LoginPage = ({ handleAuthEvt }) => {
 
   return (
     <main className={styles.container}>
-      <h1>Log In</h1>
+      <h1>LOG IN</h1>
       <p className={styles.message}>{message}</p>
       <form autoComplete="off" onSubmit={handleSubmit} className={styles.form}>
         <label className={styles.label}>
